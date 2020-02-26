9ebe59e504e3674d05c506d62f9d094c865cf991
devincalmt <devincalimto@gmail.com>
2020-02-26 08:58:22 +0700

    test
    test

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $app = new Slim\App($configs);
 
 /* ROUTES */
 $app->get('/', function ($request, $response) {
-	return "Lanjutkan!";
+	return "Test!";
 });
 
 $app->post('/', function ($request, $response)
