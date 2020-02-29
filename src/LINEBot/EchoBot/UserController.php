26ebecf9aecbc5aedc3057a77d4362ee425f5c76
devincalimto@gmail.com <devincalimto@gmail.com>
2020-02-29 18:40:26 +0700

    change

--- src/LINEBot/EchoBot/UserController.php
+++ src/LINEBot/EchoBot/UserController.php
@@ -49,44 +49,7 @@ class UserController
     }
 
     public function displayHelp(Request $req, Response $res){
-        /** @var \LINE\LINEBot $bot */
-        $bot = $this->bot;
-        /** @var \Monolog\Logger $logger */
-        $logger = $this->logger;
-
-        $signature = $req->getHeader(HTTPHeader::LINE_SIGNATURE);
-        if (empty($signature)) {
-            return $res->withStatus(400, 'Bad Request');
-        }
-
-        // Check request with signature and parse request
-        try {
-            $events = $bot->parseEventRequest($req->getBody(), $signature[0]);
-        } catch (InvalidSignatureException $e) {
-            return $res->withStatus(400, 'Invalid signature');
-        } catch (InvalidEventRequestException $e) {
-            return $res->withStatus(400, "Invalid event request");
-        }
-
-        foreach ($events as $event) {
-            if (!($event instanceof MessageEvent)) {
-                $logger->info('Non message event has come');
-                continue;
-            }
-
-            if (!($event instanceof TextMessage)) {
-                $logger->info('Non text message has come');
-                continue;
-            }
-
-            $replyText = $event->getText();
-            $logger->info('Reply text: ' . $replyText);
-            $resp = $bot->replyText($event->getReplyToken(), $replyText);
-            $logger->info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
-        }
-
-        $res->write('OK');
-        return $res;
+        echo "a";
     }
 
     public function help(Request $req, Response $res){
