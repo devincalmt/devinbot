79b3429cc4ee9ce097823d6a0ac0ea55896c17ea
devincalmt <devincalimto@gmail.com>
2020-02-29 14:09:49 +0700

    database

--- src/LINEBot/EchoBot/Route.php
+++ src/LINEBot/EchoBot/Route.php
@@ -34,12 +34,11 @@ class Route
         $app->get('/', function (Request $req, Response $res){
 //            $res->getBody()->write(":DD");
 //            return $res;
-            $servername = getenv("SERVERNAME");
-            $username = getenv("USERNAME");
-            $password = getenv("PASSWORD");
-            $dbname = getenv("DBNAME");
+            $servername = "us-cdbr-iron-east-04.cleardb.net";
+            $username = "b1f3fa9bda05bb";
+            $password = "10d0741f";
+            $dbname = "heroku_fdb27654ad74a1b";
 
-            var_dump($servername);
 // Create connection
             $conn = mysqli_connect($servername, $username, $password, $dbname);
 
@@ -47,7 +46,9 @@ class Route
             if (!$conn) {
                 die("Connection failed: " . mysqli_connect_error());
             }
-            echo "Connected successfully!";
+            echo "Connected successfully";
+
+            $this->test();
         });
 
         $app->post('/', function (Request $req, Response $res) {
@@ -90,5 +91,9 @@ class Route
             $res->write('OK');
             return $res;
         });
+    }
+
+    public function test(){
+        echo 'a';
     }
 }
